1d055970dc6c267d8faf00782b535af60cb1422a
Yousef-frhat <youseffrahat90@gmail.com>
2026-06-25 17:34:15 +0300

    feat: add Sentry error tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-// No i18n import needed, we use LanguageContext
+
+Sentry.init({
+  dsn: "https://9ec3094913aa8c7ee426b9316c245e2e@o4511626487791616.ingest.de.sentry.io/4511626520952912",
+  environment: import.meta.env.MODE,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
